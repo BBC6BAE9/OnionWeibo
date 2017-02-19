488f1e636832d46751382530cd3e72f111a55a0c
BBC6BAE9 <chinahuanghong@gmail.com>
2017-02-19 11:35:07 +0800

    调整setupUI的加载时机以强行获得ScrollView的尺寸

--- EasyWeibo/EasyWeibo/Classes/View/Main/Compose/ComposeType/WBComposeTypeView.swift
+++ EasyWeibo/EasyWeibo/Classes/View/Main/Compose/ComposeType/WBComposeTypeView.swift
@@ -10,6 +10,20 @@ import UIKit
 
 /// 撰写微博类型视图
 class WBComposeTypeView: UIView {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    // 定义按钮的数组
+    private let buttonInfo = [["imageName":"tabbar_compose_idea","title":"文字"],
+                              ["imageName":"tabbar_compose_idea","title":"照片／视频"],["imageName":"tabbar_compose_idea","title":"长微博"],["imageName":"tabbar_compose_idea","title":"签到"],["imageName":"tabbar_compose_idea","title":"点评"],["imageName":"tabbar_compose_idea","title":"更多"],
+                              
+                              ["imageName":"tabbar_compose_idea","title":"好友圈"],["imageName":"tabbar_compose_idea","title":"微博相机"],["imageName":"tabbar_compose_idea","title":"音乐"],
+                              ["imageName":"tabbar_compose_idea","title":"拍摄"],
+                                                            ]
+    
+    
     //有了XIB这个地方就不用留了
     //    override init(frame: CGRect) {
     //        super.init(frame: frame)
@@ -30,7 +44,7 @@ class WBComposeTypeView: UIView {
         let v = nib.instantiate(withOwner: nil, options: nil)[0] as! WBComposeTypeView
         //XIB默认加载时600x600
         v.frame = UIScreen.main.bounds
-        
+        v.seupUI()
         return v
         
     }
@@ -45,14 +59,20 @@ class WBComposeTypeView: UIView {
         vc.view.addSubview(self)
         
     }
-    override func awakeFromNib() {
-        seupUI()
-    }
+    
     
     /// 按钮点击监听方法
     func clickButton(){
+        
+        print("点我......")
+    }
     
-    print("点我......")
+    
+    
+    @IBAction func close(_ sender: Any) {
+        
+        removeFromSuperview()
+        
     }
 }
 
@@ -60,19 +80,25 @@ class WBComposeTypeView: UIView {
 
 // MARK: - 让extension所有的函数都私有
 extension WBComposeTypeView{
-
-    func seupUI(){
     
-        let btn = WBComposeTypeButton.composeTypeButton(imageName: "tabbar_compose_more", title: "have a try")
+    func seupUI(){
         
-        btn.frame = CGRect(x: 100 , y: 100, width: 100, height: 100)
-        //添加监听方法
-        btn.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
+//        let btn = WBComposeTypeButton.composeTypeButton(imageName: "tabbar_compose_more", title: "have a try")
+//        
+//        btn.frame = CGRect(x: 100 , y: 100, width: 100, height: 100)
+//        //添加监听方法
+//        btn.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
+//        
+//        addSubview(btn)
         
-        addSubview(btn)
+        //强行更新布局，拿到scrollView按钮
+        layoutIfNeeded()
+        
+        //向视图添加按钮
+        let v = UIView()
         
         
     }
-
-
+    
+    
 }
